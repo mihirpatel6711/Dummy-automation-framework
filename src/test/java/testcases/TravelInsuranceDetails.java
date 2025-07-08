3476c8d64094c8e74797c926be0c55ae2249a062
Reddy <2406710@cognizant.com>
2025-07-08 15:02:17 +0530

    code added by varshitha

--- src/test/java/testcases/TravelInsuranceDetails.java
+++ src/test/java/testcases/TravelInsuranceDetails.java
@@ -14,7 +14,53 @@ import base.TestBase;
 import util.ReadExcel;
 
 public class TravelInsuranceDetails extends TestBase {
-
+    public TravelInsuranceDetails() throws Exception {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    TravelInsurancePage travel = new TravelInsurancePage();
+    HomePage home = new HomePage();
+    ReadExcel re = new ReadExcel();
+     String userDir = System.getProperty("user.dir");
+    String filePath = userDir + "\\src\\test\\java\\resources\\TravelInsurance.xlsx";
+    @BeforeClass
+   
+    public void setup()throws Exception {
+        invokeBrowser();
+        driver.navigate().to("https://www.policybazaar.com/");
+       
+    }    
+    @Test(priority=1)
+    public void opentravelinsurancepage() throws InterruptedException {
+        home.clickTravelInsurance();
+    }
+   
+   
+    @Test(priority=2)
+    public void student() throws Exception {
+        travel.clickStudent();
+        logger = report.createTest("Travel Insurance Test");
+        logger.log(Status.PASS, "TestCase Passed");
+        logger.log(Status.INFO, "Clicking student tab");
+    }
+    @Test(priority=3)
+    public void dest() throws IOException {
+        travel.Destination(re.readexcel(0,filePath ,0));
+        logger.log(Status.PASS, "TestCase Passed");
+        logger.log(Status.INFO, "Filling Destination");
+    }
+    @Test(priority=4)
+    public void age() throws IOException {
+        travel.traveller(re.readexcel(0, filePath, 1),re.readexcel(0, filePath, 2));
+        logger.log(Status.PASS, "TestCase Passed");
+        logger.log(Status.INFO, "Filling Age");
+    }
+    @Test(priority=5)
+    public void start() throws InterruptedException {
+        travel.startdate();
+        logger.log(Status.PASS, "TestCase Passed");
+        logger.log(Status.INFO, "Choosing Start Date");
+    }
 	
 }
 
